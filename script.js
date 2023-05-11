ec99a2418089e66718e1050ee8e4e4fb2b93e4b5
Rai11ox <ddemin024@gmail.com>
2023-05-11 14:43:55 +0300

    best

--- script.js
+++ script.js
@@ -6,36 +6,8 @@ alert(b);
 let c = prompt("How old are u?", 15);
 console.log(c);
 document.write(c);*/
-/*let d = 0;
+let d = 0;
 while(d <=100){
     console.log(d);
     d++;  
 };
-let i = 0;
-do{
-    if(i ===0){
-        console.log(i + ' - це нуль')
-    }
-    else if(i % 2 === 0){
-        console.log(i + " - парне число")
-    }
-    else  {
-        console.log(i + " - непарне число")
-    }
-    i++;
-
-}
-while (i <= 10);*/
-/*let numb = 10000;
-let counter = 0;
-do{
-    numb /= 2;
-    counter++;
-}
-while (numb >= 50);
-let result = numb;
-console.log("Result:" + result);
-console.log("Count:" + counter);*/
-
-let a = 1,2,12;
-
